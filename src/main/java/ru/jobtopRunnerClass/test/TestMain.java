ce9575fab6c86307bdb3b1546bc167819074b272
igor <9999999593a@gmail.com>
2018-02-25 23:19:13 +0300

    changes from 25/02/2018''''

--- src/main/java/ru/jobtopRunnerClass/test/TestMain.java
+++ src/main/java/ru/jobtopRunnerClass/test/TestMain.java
@@ -67,6 +67,7 @@ public class TestMain extends PreCondChrome {
                   listing.getListingCardTitle().click();
                   Assert.assertTrue(insideCard.getInsCardSpecAvto().has(Condition.text(x.getText())));
                   System.out.println("11hhhhhhhh");
+                  System.out.println("11hhhhhhhh");
              }
 
 
